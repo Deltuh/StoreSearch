6e9774343c99554f5cebae3482907bed84d8c7ed
Deltuh <deltfps@gmail.com>
2022-02-18 08:02:13 -0500

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Deividas Sipavicius on 2/18/22.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
